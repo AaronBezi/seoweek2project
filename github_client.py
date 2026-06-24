7edc9c4f7e00cf82ca38cd5d94f0d042976e8a89
Aaron Bezi <aaronbezi4@gmail.com>
2026-06-24 11:49:27 -0400

    Update github_client.py

---
+++ github_client.py
@@ -0,0 +1,19 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+
+
+def parse_github_url(url):
+    pass
+
+
+def get_repo_info(owner, repo):
+    pass
+
+
+def get_readme(owner, repo):
+    pass
